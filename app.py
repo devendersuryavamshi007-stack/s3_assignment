95201feb84dd85751b80e9a018a983d8009e21a5
devsurya <devender.suryavamshi007@gmail.com>
2025-09-05 11:57:43 +0530

    test done

--- app.py
+++ app.py
@@ -263,4 +263,4 @@ def smart_suggestions():
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
